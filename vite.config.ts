57540334e4ed5f5058f29ac12fe1b1d2ab670a60
Alex Chubchik <very_vazhno@mail.ru>
2025-08-29 16:05:51 +0300

    Split Vite and Vitest configs for proper deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import type { UserConfig as VitestUserConfig } from "vitest/config"
 
 export default defineConfig({
 	plugins: [react()],
-	base: "/mindbox_todo/",
-	// @ts-ignore
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./vitest.setup.ts",
+	base: "/Mindbox_Todo/",
+	build: {
+		outDir: "dist",
+		assetsDir: "assets",
 	},
-} as VitestUserConfig)
+})
